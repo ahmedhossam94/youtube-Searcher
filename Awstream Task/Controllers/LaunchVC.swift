9def5fd0a8b3a93560c30ad1b9c62cc5c446b5b7
ahmedhossam94 <medo_ah94@hotmail.com>
2019-03-22 09:24:28 +0200

    intial Comment

---
+++ Awstream Task/Controllers/LaunchVC.swift
@@ -0,0 +1,30 @@
+//
+//  LaunchVC.swift
+//  Awstream Task
+//
+//  Created by ahmed on 3/21/19.
+//  Copyright © 2019 Ahmed Hossam. All rights reserved.
+//
+
+import UIKit
+
+class LaunchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
